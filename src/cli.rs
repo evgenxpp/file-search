70c3afb85fbb3f5e003f8cb21b5df7bc6a5cc84a
Evgeny Ivanov 🐧 <evivan@microsoft.com>
2025-06-27 12:28:32 +0100

    remove defaults

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,6 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 pub struct Cli {
-    #[arg(long, default_value = "D:\\tmp")]
+    #[arg(long)]
     pub path: String,
 }
